a4b5214c01ac2264f3c5df9f761a9beb42697878
Manuel Dalla Lana <manuel@pepeverde.agency>
2018-07-10 10:01:29 +0200

    Implement Zigra_registryInterface on Registry

--- src/Pepeverde/Registry.php
+++ src/Pepeverde/Registry.php
@@ -2,54 +2,106 @@
 
 namespace Pepeverde;
 
-use Aura\Session\Session;
-use Aura\Session\SessionFactory;
+use Zigra_RegistryInterface;
 
-class Registry
+/**
+ * Class Registry
+ */
+class Registry implements Zigra_RegistryInterface
 {
-    /** @var Session $session */
-    public static $session;
+    private static $instance;
+    private static $vars = [];
 
     /**
-     * @return \Aura\Session\Segment
+     * @return Registry
      */
     public static function getRegistry()
     {
-        $session_factory = new SessionFactory;
-        static::$session = $session_factory->newInstance($_COOKIE);
+        if (self::$instance === null) {
+            self::$instance = new static();
+        }
 
-        return static::$session->getSegment('zigra\registry');
+        return self::$instance;
     }
 
-    /**
-     * @param string $key
-     * @param mixed $value
-     */
-    public static function set($key, $value)
+    private function __clone()
     {
-        $registry = self::getRegistry();
-        $registry->set($key, $value);
+    }
+
+    private function __construct()
+    {
     }
 
     /**
      * @param string $key
      * @param mixed $alt
-     * @return mixed
+     * @return mixed|null
      */
     public static function get($key, $alt = null)
     {
-        $registry = self::getRegistry();
+        if (self::has($key)) {
+            return self::$vars[$key];
+        }
 
-        return $registry->get($key, $alt);
+        return $alt;
     }
 
     /**
      * @param string $key
+     * @param mixed $value
+     * @return bool
+     */
+    public static function set($key, $value)
+    {
+        if (!self::has($key)) {
+            self::$vars[$key] = $value;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Add a variable in the $key array
+     *
+     * @param string $key the variable's name
+     * @param string $value the variable's value
      * @return mixed
+     */
+    public static function add($key, $value)
+    {
+        // TODO: Implement add() method.
+        if (self::has($key) && is_array(self::$vars[$key])) {
+            self::$vars[$key][] = $value;
+        }
+    }
+
+    /**
+     * @param string $key
+     * @return bool
      */
     public static function has($key)
     {
-        return isset($_SESSION['zigra\registry'][$key]);
+        if (is_string($key)) {
+            return array_key_exists($key, self::$vars);
+        }
+
+        //throw new Exception('Key must be a string')
+        return false;
+    }
+
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public static function remove($key)
+    {
+        if (self::has($key)) {
+            unset(self::$vars[$key]);
+        }
+
+        return true;
     }
 
     /**
@@ -57,10 +109,54 @@ class Registry
      */
     public static function getAll()
     {
-        if (isset($_SESSION['zigra\registry'])) {
-            return $_SESSION['zigra\registry'];
+        if (!empty(self::$vars)) {
+            return self::$vars;
         }
 
         return [];
+    }
+
+    /**
+     * @return array
+     */
+    public static function getKeys()
+    {
+        if (!empty(self::$vars)) {
+            return array_keys(self::$vars);
+        }
+
+        return [];
+    }
+
+    public static function clear()
+    {
+        self::$vars = [];
+    }
+
+    /**
+     * @param string $key
+     * @return mixed|null
+     */
+    public function __get($key)
+    {
+        return self::get($key);
+    }
+
+    /**
+     * @param string $key
+     * @param mixed $value
+     */
+    public function __set($key, $value)
+    {
+        self::set($key, $value);
+    }
+
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public function __isset($key)
+    {
+        return self::has($key);
     }
 }
