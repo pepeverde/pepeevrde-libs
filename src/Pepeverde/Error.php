218930a9fd896843d9bd06b20e807eeea2c5dec0
Manuel Dalla Lana <manuel@dallalana.it>
2019-11-04 12:14:35 +0100

    Implement Sentry SDK 2.0.4 in Error class

--- src/Pepeverde/Error.php
+++ src/Pepeverde/Error.php
@@ -2,106 +2,94 @@
 
 namespace Pepeverde;
 
-use Raven_Client;
 use Zigra_Exception;
 
 class Error
 {
-    /** @var Raven_Client */
-    protected static $ravenClient;
-
-    /** @var array */
-    private static $sentryConfig;
-
-    /**
-     * @param array|null $ravenConfig
-     * @param string $appVersion
-     * @param string $environment
-     * @param array $extra
-     * @return Raven_Client
-     */
-    public static function getRavenInstance($ravenConfig = null, $appVersion = 'dev', $environment = 'development', array $extra = [])
+    private static function configureSentry(
+        array $sentryConfig,
+        $appVersion,
+        $environment,
+        array $extra,
+        bool $send_default_pii
+    ): void
     {
-        if (null === self::$ravenClient) {
-            self::$sentryConfig = $ravenConfig;
-            if ($ravenConfig === null) {
-                self::$sentryConfig = Registry::get('config.sentry');
-            }
+        \Sentry\init([
+            'dsn' => $sentryConfig['sentry-server'],
+            'release' => $appVersion,
+            'environment' => $environment,
+            'send_default_pii' => $send_default_pii,
+        ]);
 
-            if (null === $ravenConfig) {
-                throw new \RuntimeException('No Sentry configuration available');
-            }
-
-            $sentryServer = self::$sentryConfig['sentry-server'];
-
-            $sentryClientConfig = [
-                // pass along the version of your application
-                'release' => $appVersion,
-                // pass along your environment
-                'environment' => $environment,
-                'tags' => [
-                    'php_version' => PHP_VERSION
-                ],
-                'extra' => $extra,
-            ];
-
-            self::$ravenClient = new Raven_Client($sentryServer, $sentryClientConfig);
-        }
-
-        return self::$ravenClient;
+        \Sentry\configureScope(static function (\Sentry\State\Scope $scope) use ($extra): void {
+            $scope->setExtras($extra);
+        });
     }
 
     /**
-     * @param array|null $ravenConfig
-     * @param string $appVersion
-     * @param string $environment
-     * @param array $extra
-     * @throws \Raven_Exception
+     * @param array|null $sentryConfig
+     * @param string     $appVersion
+     * @param string     $environment
+     * @param array      $extra
+     * @param bool       $send_default_pii
      */
-    public static function enableErrorHandler($ravenConfig = null, $appVersion = 'dev', $environment = 'development', array $extra = [])
+    public static function enableErrorHandler(
+        $sentryConfig = null,
+        $appVersion = 'dev',
+        $environment = 'development',
+        array $extra = [],
+        bool $send_default_pii = false
+    )
     {
-        $sentryClient = self::getRavenInstance($ravenConfig, $appVersion, $environment, $extra);
-        $sentryClient->install();
+        if (null === $sentryConfig) {
+            throw new \RuntimeException('No Sentry configuration available');
+        }
+
+        self::configureSentry($sentryConfig, $appVersion, $environment, $extra, $send_default_pii);
     }
 
     /**
      * @param \Exception $e
-     * @param bool $display
-     * @param array $data
+     * @param bool       $display
+     * @param array      $data
      */
     public static function report(\Exception $e, $display = true, array $data = [])
     {
-        $raven = self::getRavenInstance();
-        $event_id = $raven->getIdent($raven->captureException($e, $data));
-        if ($raven->getLastError() !== null) {
-            printf('There was an error sending the event to Sentry: %s', $raven->getLastError());
+        if (!empty($data)) {
+            \Sentry\configureScope(static function (\Sentry\State\Scope $scope) use ($data): void {
+                $scope->setExtras($data);
+            });
         }
-        if ($display) {
+
+        $event_id = \Sentry\captureException($e);
+
+        $message = $e->getMessage();
+        if (null !== $event_id) {
+            $message .= ' [Event-ID: ' . $event_id . ']';
+        }
+        if (true === $display) {
             Zigra_Exception::renderError(
                 $e->getCode(),
-                $e->getMessage() . ' [Event-ID: ' . $event_id . ']'
+                $message
             );
         }
     }
 
     /**
      * @param string $message
-     * @param array $params
-     * @param array $data
-     * @param bool $stack
-     * @param null $vars
      */
-    public static function message(
-        $message,
-        array $params = [],
-        array $data = [],
-        $stack = false,
-        $vars = null
-    ) {
-        $raven = self::getRavenInstance();
-        $raven->captureMessage($message, $params, $data, $stack, $vars);
-        if ($raven->getLastError() !== null) {
-            printf('There was an error sending the event to Sentry: %s', $raven->getLastError());
-        }
+    public static function message($message)
+    {
+        \Sentry\captureMessage($message);
+    }
+
+    public static function setUser($username, $email = null)
+    {
+        \Sentry\configureScope(function (\Sentry\State\Scope $scope) use ($username, $email): void {
+            $scope->setUser([
+                'username' => $username,
+                'email' => $email
+            ]);
+        });
     }
 }
